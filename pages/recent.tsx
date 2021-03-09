875ca509095e372ccf33e3c9292dc7c58cb3eb52
Gabor Juhasz <gabor.juhasz@accretivetg.com>
2021-03-09 22:43:47 +0100

    fix: recent redirects to home with no lists

--- pages/recent.tsx
+++ pages/recent.tsx
@@ -6,6 +6,11 @@ const DbService = new LocalStorageDatabase()
 const Recent = () => {
   useEffect(() => {
     const openLists = DbService.getLists().filter((list) => list.open > 0)
+    if (!openLists.length) {
+      window.location.assign('/')
+      return
+    }
+
     openLists.sort((a, b) => (a.id < b.id ? 1 : -1))
     window.location.assign(`/list/${openLists[0].id}`)
   })
